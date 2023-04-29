247f23c116103a67aaea0a533a18c2f305a463a8
Muhammad Farhan <m.farhankz@gmail.com>
2023-04-29 12:19:58 +0500

    fix the path issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,5 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
