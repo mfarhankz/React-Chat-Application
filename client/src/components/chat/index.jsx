92e5e3d5d4ee2bb34f01d1a30d9c72abc7f7ea73
Muhammad Farhan <m.farhankz@gmail.com>
2023-04-29 13:06:20 +0500

    chat start looking good

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/header";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "mFarhankz",
+    "1234"
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100%" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
 };
 
 export default Chat;
