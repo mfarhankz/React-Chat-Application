97ec6d3da572f723a441912863bdde71733ef334
Muhammad Farhan <m.farhankz@gmail.com>
2023-05-04 13:38:53 +0500

    extra commit

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -8,14 +8,14 @@ function Header({ chat }) {
         <ChatBubbleLeftRightIcon className="icon-chat" />
         <h3 className="header-text">{chat.title}</h3>
       </div>
-      <div className="flexbetween">
+      {/* <div className="flexbetween">
         <PhoneIcon className="icon-phone" />
         {chat.description !== "⬅️ ⬅️ ⬅️" ? (
           <p className="header-text">{chat.description}</p>
         ) : (
           <p className="header-text">no chat selected</p>
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
